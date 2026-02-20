fc816057fa6a4f14364b75680c1221348a9a06d4
Putdhikarn <67070123@kmitl.ac.th>
2026-02-20 17:39:03 +0700

    feat: make the server listen to http and redirect it to https.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import 'dotenv/config'
 
 import express from 'express';
 import cors from 'cors'
+import http from 'http'
 import https from 'https'
 // import { dbQuery, dbClientQuery } from './database/db.js';
 import * as Auth from './auth/auth.js';
@@ -27,9 +28,18 @@ const options = {
     cert: fs.readFileSync('./cert/cert.pem')
 };
 
+// Redirect to  HTTPS
+app.use((req, res, next) => {
+    if (!req.secure) {
+        return res.redirect(`https://${req.headers.host}${req.url}`);
+    }
+    next();
+});
+// Cross Origin Resource Sharing
 app.use(cors())
 app.use(express.static('public'));
 app.use(express.json({ limit: "10mb" }));
+
 
 app.post('/api/echo/:msg', (req, res) => {
     const { msg } = req.params;
@@ -50,16 +60,24 @@ app.use('/api/addproduct', AddProduct.router);
 app.use('/api/deleteproduct', DeleteProduct.router);
 
 // This Should Always be at the bottom!
-app.get('/{*splat}', (req, res) => {
+app.get('/{*splat}', (req, res, next) => {
      //res.status(200).send({msg: "The REST Back End Actually works!!!"});
 	 res.status(200).sendFile(Path.join(Path.dirname(fileURLToPath(import.meta.url)), 'public/index.html'));
 });
 
-const server = https.createServer(options, app)
+const httpServer = http.createServer(options, app);
+const httpsServer = https.createServer(options, app);
 
-server.listen(
-    process.env.SERVER_PORT,
+httpServer.listen(
+    process.env.HTTP_SERVER_PORT,
     () => {
-        console.log(`Backend Running on : https://localhost:${process.env.SERVER_PORT}`);
+        console.log(`Backend Running on : https://localhost:${process.env.HTTP_SERVER_PORT}`);
+    }
+);
+
+httpsServer.listen(
+    process.env.HTTPS_SERVER_PORT,
+    () => {
+        console.log(`Backend Running on : https://localhost:${process.env.HTTPS_SERVER_PORT}`);
     }
 );
